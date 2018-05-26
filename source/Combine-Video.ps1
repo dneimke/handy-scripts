9773868b1f92fb9275b9189f41965836a48b7b6e
Darren Neimke <darren.neimke@gmail.com>
2018-05-27 09:23:53 +0930

    reverts to using temp files

--- source/Combine-Video.ps1
+++ source/Combine-Video.ps1
@@ -1,23 +1,19 @@
 
-$InputPath = "C:\users\darre\Videos\2018\PLW_R6_BHCvPADHC"    
+$InputFolderPath = "C:\users\darre\Videos\2018\PLW_R6_BHCvPADHC"    
 $Extension = "m2ts"
-$tmpFile = "C:\temp\temp.txt"
 $OutputFile = 'C:\temp\foo.m2ts'
 
 $tmp = New-TemporaryFile
 
-
-
 Write-Host "Created temp file $($tmp.FullName)"
 
-$stream = [System.IO.StreamWriter] $tmpFile # $tmp.FullName
+$stream = [System.IO.StreamWriter] $tmp.FullName
 
-gci -Path $InputPath -filter "*.$Extension" | % {
+gci -Path $InputFolderPath -filter "*.$Extension" | % {
     Write-Host "Adding file $InputPath\$_"
-
     $stream.WriteLine("file '$InputPath\$_'")
 }
 
 $stream.close()
 
-ffmpeg -f concat -safe 0 -i $tmpFile -c copy $OutputFile
+ffmpeg -f concat -safe 0 -i $tmp.FullName -c copy $OutputFile
